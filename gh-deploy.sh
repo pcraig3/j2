fa1c7b36f1457308b089e070b1a178f1b1676977
pcraig3 <paul_craig_16@hotmail.com>
2018-03-10 23:29:57 -0500

    tinker with deploy script
    one-line deploys is what we want
    
    script does this:
    - check nothing is returned by 'git status --porcelain"
    - build and export static html (gets put into '/out' directory)
    - add everything and commit it
    - make a new 'gh-pages' branch from the '/out' directory
    - push 'gh-pages' up to github, overwriting the old branch
    - delete the local 'gh-pages' branch
    - reset the last commit
      - i don't see the point in polluting the git history with massive
        generated html files

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-if [ -z "$1" ]
+if [ -n "$(git status --porcelain)" ]
 then
-  echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "Error: cannot deploy until all changes are committed."
   exit 1
+# else
+#   echo "no changes";
 fi
-git subtree push --prefix $1 origin gh-pages
+
+npm run be # build and export static html files
+git add .
+git commit -m "build and export static html files"
+git subtree split --prefix out -b gh-pages # create a local gh-pages branch containing the splitted output folder
+git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
+git branch -D gh-pages # delete the local gh-pages because you will need it: ref
+git reset --hard HEAD~1 # remove the commit where the static html was exported. we don't need this in the history
