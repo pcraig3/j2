6de4e7d93bf88bcfdc0dbd4a2dc17f4f7b0c9ee1
pcraig3 <paul_craig_16@hotmail.com>
2018-03-12 00:51:59 -0400

    move .nojekyll to /out, stop git resets

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -13,4 +13,4 @@ git commit -m "build and export static html files"
 git subtree split --prefix out -b gh-pages # create a local gh-pages branch containing the splitted output folder
 git push -f origin gh-pages:gh-pages # force the push of the gh-pages branch to the remote gh-pages branch at origin
 git branch -D gh-pages # delete the local gh-pages because you will need it: ref
-git reset --hard HEAD~1 # remove the commit where the static html was exported. we don't need this in the history
+# git reset --hard HEAD~1 # remove the commit where the static html was exported. we don't need this in the history
--- out/placeholder.md
+++
@@ -1,1 +0,0 @@
-We want to keep the '/out' folder around, but not keep track of its contents
