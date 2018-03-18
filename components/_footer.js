2494a32de6b49c9c18f89ba7bb8de2f5131f80cd
pcraig3 <paul_craig_16@hotmail.com>
2018-03-18 17:20:25 -0400

    hide footer on small screen
    it's just a vanity thing anyway

--- components/_footer.js
+++ components/_footer.js
@@ -1,23 +1,31 @@
 import Link from 'next/link'
 import { css } from 'react-emotion'
-import { fontSizes, spacing } from './__styles'
+import { fontSizes, spacing, mq } from './__styles'
 
 const footerStyles = css`
   position: fixed;
-    bottom: ${spacing.xxs}px;
-    right: ${spacing.xl + spacing.xxs}px; /* 5px extra to account for padding */
-    font-size: ${fontSizes.xs}rem;
-    font-family: sans-serif;
-    text-transform: lowercase;
-    opacity: 0.2;
-  }
+  bottom: ${spacing.xxs}px;
+  right: ${spacing.xl + spacing.xxs}px; /* 5px extra to account for padding */
+  font-size: ${fontSizes.xs}rem;
+  font-family: sans-serif;
+  text-transform: lowercase;
+  opacity: 0.2;
+  display: none;
+
   &:hover {
-    opacity: .5;
+    opacity: 0.5;
   }
-  a {
+
+  ${mq.sm(css`
+    display: block;
+  `)};
+
+  a,
+  a:hover,
+  a:focus {
     color: #d43b1d;
   }
-  `
+`
 
 export default () => (
   <footer className={footerStyles}>
